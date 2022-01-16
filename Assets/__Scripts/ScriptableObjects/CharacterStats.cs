35bfafeb515124b28a7ac6737104be250ae0a368
dmdylan <51095343+dmdylan@users.noreply.github.com>
2022-01-16 13:44:34 -0500

    Character Setup

--- Assets/__Scripts/ScriptableObjects/CharacterStats.cs
+++ Assets/__Scripts/ScriptableObjects/CharacterStats.cs
@@ -8,36 +8,31 @@ public class CharacterStats : ScriptableObject
     [Header("Primary Stats")]
     [SerializeField] private int health;
     [SerializeField] private int energy;
-
-    [Header("Secondary Stats")]
-    [SerializeField] private int vitality;
     [SerializeField] private int strength;
-    [SerializeField] private int stamina;
-    [SerializeField] private int dexterity;
+    [SerializeField] private int ability;
+
+    //[Header("Secondary Stats")]
+    //[SerializeField] private int vitality;
+    //[SerializeField] private int stamina;
+    //[SerializeField] private int dexterity;
 
     [Header("Other Stats")]
     [SerializeField] private int speed;
-    [SerializeField] private int luck;
+    [SerializeField] private float luck;
 
     [Header("Defensive Stats")]
     [SerializeField] private int dodge;
     [SerializeField] private int block;
 
-    [Header("Stat Bonuses")]
-    [SerializeField] private float healthBonus;
+    //[Header("Stat Bonuses")]
+    //[SerializeField] private float healthBonus;
 
-    public int Health
-    {
-        get
-        {
-            return health;
-        }
-
-        set
-        {
-            health = health + (vitality * (int)1) + (strength * (int)1);
-        }
-    }
-
+    public int Health => health;
     public int Energy => energy;
+    public int Strength => strength;
+    public int Ability => ability;
+    public int Speed => speed;
+    public float Luck => luck;
+    public int Dodge => dodge;
+    public int Block => block;
 }
